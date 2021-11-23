9ef6dfe63cc43a14f7b32e025112de791c018705
Андрей Гребенюк <andreygrebenyuk04@gmail.com>
2021-11-23 19:23:09 +0200

    Добавьте файлы проекта.

---
+++ 3.3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //3.3, 2-й вариант, Гребенюк
+            Console.WriteLine("Введите значение x (в радианах): ");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите колчество повторений цикла n: ");
+            int n = int.Parse(Console.ReadLine());
+            double S = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                S =+ Math.Cos(i * x) / i;
+            }
+            Console.WriteLine("Summ = " + S);
+        }
+    }
+}
